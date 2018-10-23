e3f4dd1723143e0c100fb68abac63935f889c309
Diana Cepeda <dianacepeda@MacBook-Pro-de-Diana.local>
2018-10-23 18:25:33 -0500

    Login, SignUp

---
+++ src/components/SignupSection.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Dimensions from 'Dimensions';
+import {StyleSheet, View, Text} from 'react-native';
+
+export default class SignupSection extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <TouchableOpacity onPress={this.props.change('signup')} style={styles.touchable} activeOpacity={0.6}>
+            <Text style={styles.createAccount}>Crear cuenta</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.props.change('forgot')} style={styles.touchable} activeOpacity={0.6}>
+            <Text style={styles.forgotPassword}>Olvidé mi contraseña</Text>
+          </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const DEVICE_WIDTH = Dimensions.get('window').width;
+const DEVICE_HEIGHT = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    top: 65,
+    width: DEVICE_WIDTH,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  text: {
+    color: 'white',
+    backgroundColor: 'transparent',
+  },
+  createAccount: {
+    color:'#ffffffEE',
+    textAlign: 'center',
+    fontSize: totalSize(2),
+    fontWeight: '600',
+  },
+  forgotPassword: {
+    color:'#ffffffEE',
+    textAlign: 'center',
+    fontSize: totalSize(2),
+    fontWeight: '600',
+  }
+});
