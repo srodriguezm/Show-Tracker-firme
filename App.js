bb2708eb1ece6efe03a4984abb80beacdc5ae18c
santiago rodriguez matiz <s.rodriguezm@uniandes.edu.co>
2018-10-24 16:03:19 -0500

    ya

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * import DashboardScreen from './screens/DashboardScreen';
+ */
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, ImageBackground, Button, TouchableOpacity} from 'react-native';
+import {createStackNavigator} from 'react-navigation';
+import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
+import SignUpScreen from './screens/SignUpScreen'
+import DashboardScreen from './screens/DashboardScreen'
+import {Fonts} from './src/util/Fonts';
+export default class App extends Component {
+
+  render() {
+    return (
+      <AppStackNavigator style={styles.containerp}/>
+    );
+  }
+}
+
+const AppStackNavigator = createStackNavigator({
+  Home:HomeScreen,
+  Login:LoginScreen,
+  Dashboard:DashboardScreen,
+  SignUp:SignUpScreen
+})
+
+const styles = StyleSheet.create({
+  containerp:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'#010102',
+  },
+});
